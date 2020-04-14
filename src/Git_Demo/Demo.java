b28e391dc9c28252b9153f904e7d92d82f1d7f4f
sopalanga <sopalanga@gmail.com>
2020-04-14 14:35:29 -0700

    sri branch commit

--- src/Git_Demo/Demo.java
+++ src/Git_Demo/Demo.java
@@ -10,12 +10,8 @@ System.out.println("Im making changes in sri branch");
 //i have changed some thing
 //margi branch
 System.out.println("im on branch nidhi kindly merge my branch");
-<<<<<<< HEAD
-
-
 //i am nidhi...i want to marge sri
 System.out.println("Kindly merge me");
-=======
->>>>>>> sri
+
 }
 }
