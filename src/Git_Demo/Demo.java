ccce123765afdc8e90d82631d4af9d98cf975012
sopalanga <sopalanga@gmail.com>
2020-04-14 08:35:50 -0700

    commiting sri branch changes

--- src/Git_Demo/Demo.java
+++ src/Git_Demo/Demo.java
@@ -1,5 +1,12 @@
 package Git_Demo;
 
 public class Demo {
+	public static void main(String[] args) {
+		/*i have 
+	}*/
+System.out.println("Im learning git today");
+System.out.println("Im making changes in sri branch");
 
+//i have changed some thing
+}
 }
